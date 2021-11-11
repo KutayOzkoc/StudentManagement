e0984dd22b7c814b84a3b638ed0ad19d4b2a61d7
kutay.ozkoc <kutay.ozkoc@netas.com.tr>
2021-11-11 17:45:19 +0300

    corona screen end

--- student_management_fend/src/components/CoronaScreen.js
+++ student_management_fend/src/components/CoronaScreen.js
@@ -1,24 +1,38 @@
 import React, {useState} from 'react';
 import axios from "axios";
+import {useHistory} from "react-router-dom";
 
 
 function CoronaScreen(props){
-    const [covDate,setCovDate] = useState("");
+    const [covDate,setCovDate] = useState([]);
+    const [date,setDate] = useState("");
+    const [patients,setPatients] = useState("");
+    const [deaths,setDeaths] = useState("");
+    const [recoverd,setRecoverd] = useState("");
+    const [totalDeaths,setTotalDeaths] = useState("");
+    const history = useHistory();
+
+    const backButton = async ()=>{
+        await history.push({
+            pathname:`/`,
+        })
+    }
 
     const fetchData = () => {
         return axios.get("https://raw.githubusercontent.com/ozanerturk/covid19-turkey-api/master/dataset/timeline.json")
             .then((response) =>{
-                setCovDate(response.data);
+                setDate(response.data[covDate].date);
+                setPatients(response.data[covDate].patients);
+                setDeaths(response.data[covDate].deaths);
+                setRecoverd(response.data[covDate].totalRecovered);
+                setTotalDeaths(response.data[covDate].totalDeaths);
             });}
-
-
     return (
         <div>
-
+            <button onClick={backButton} className="btn btn-warning mt-lg-2 mb-2">Back to the main screen</button>
             <input className="w-33 p-3 mt-lg-3 d-inline-block"
                    type="text"
                    name="covDate"
-                   value={covDate}
                    onChange={(e)=>{setCovDate(e.target.value)}}
                    placeholder="Tarih değerini giriniz (örn: 10/10/2020)" />
             <button onClick={fetchData} className="btn btn-primary mt-lg-2 ml-2 d-inline-block"> DATA BUTTON</button>
@@ -35,13 +49,13 @@ function CoronaScreen(props){
                 </thead>
                 <tbody>
                 {
-                            <tr>
-                                <td> {covDate.date}</td>
-                                <td> {covDate.patients}</td>
-                                <td> {covDate.deaths}</td>
-                                <td> {covDate.totalRecoverd}</td>
-                                <td> {covDate.totalDeaths}</td>
-                            </tr>
+                    <tr>
+                        <td> {date}</td>
+                        <td> {patients}</td>
+                        <td> {deaths}</td>
+                        <td> {recoverd}</td>
+                        <td> {totalDeaths}</td>
+                    </tr>
                 }
                 </tbody>
             </table>
